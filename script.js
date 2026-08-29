662c376c066dfaad5c68461a05f2e31635c8cd12
fakashaf-netize <fakashaf@gmail.com>
2026-08-29 17:07:40 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function goToTop() {
+window.scrollTo(0, 0);
+}
+
+function orderItem(item) {
+alert(item + " added to your order! 🎀");
+}
+
+function sayHello() {
+alert("Thank you for visiting Hello Kitty's Bakery! 💕");
+}
